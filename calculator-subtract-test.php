e58408cb8946a99d7c482615369b3ac085c6c4db
Prodan Boyadzhiev <103401596+boyadpro000@users.noreply.github.com>
2022-06-09 18:19:56 +0000

    adds

--- calculator-subtract-test.php
+++ calculator-subtract-test.php
@@ -6,7 +6,7 @@ include 'Calculator.php';
 class TestOfCalculator extends UnitTestCase {
 	function testSubtract() {
 	  $myCalc = new Calculator();
-	  $this->assertEqual($myCalc->add(4.2,2.1), 2.1, "4.2 - 2.1");
-	  $this->assertEqual($myCalc->add(4,2), 2, "4 - 2");
+	  $this->assertEqual($myCalc->Subtract(4.2,2.1), 2.1, "4.2 - 2.1");
+	  $this->assertEqual($myCalc->Subtract(4,2), 2, "4 - 2");
 	}
 	
